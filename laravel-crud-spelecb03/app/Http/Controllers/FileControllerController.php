52e6d935ff5ef7188517f3af27a5171dfa078566
louise710 <louisecanedo710@gmail.com>
2025-06-23 12:30:31 +0800

    try

---
+++ laravel-crud-spelecb03/app/Http/Controllers/FileControllerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FileController;
+use App\Http\Requests\StoreFileControllerRequest;
+use App\Http\Requests\UpdateFileControllerRequest;
+
+class FileControllerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFileControllerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FileController $fileController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FileController $fileController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFileControllerRequest $request, FileController $fileController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FileController $fileController)
+    {
+        //
+    }
+}
