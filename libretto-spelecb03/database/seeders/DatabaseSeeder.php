c078bb01e8079e882ac20de4c70b3ef6589fb706
louise710 <louisecanedo710@gmail.com>
2025-06-25 12:40:41 +0800

    tbc controllers, reqs, views

--- libretto-spelecb03/database/seeders/DatabaseSeeder.php
+++ libretto-spelecb03/database/seeders/DatabaseSeeder.php
@@ -2,22 +2,24 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Genre;
+use App\Models\Review;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        \App\Models\Author::factory(10)->create()->each(function ($author) {
+            $books = \App\Models\Book::factory(3)->create(['author_id' => 
+            $author->id]);
+            $books->each(function ($book) {
+                $genres = \App\Models\Genre::factory(2)->create();
+                $book->genres()->attach($genres);
+                \App\Models\Review::factory(5)->create(['book_id' => $book->id]);
+            });
+        });
     }
 }
